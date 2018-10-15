6a84bcc0f62cf45ca465c5616b8df75536020f88
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-10-15 10:59:32 -0400

    mino changes to <a>S

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -89,9 +89,7 @@ $(document).ready(function () {
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
 });
-$(document).ready(function(){
-    $('.carousel').carousel();
-  });
+
 
 // ++WHO AM I++ INPUT DATA FROM INPUT FEILD SHARED TO FIREBASE
   var config = {
